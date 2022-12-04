6e15ae0f1fe34b5d0ca6cfc6cd7546c66cae2797
awcheung1213 <awcheung1213@gmail.com>
2022-12-03 21:03:53 -0800

    fixed exposed key

---
+++ dist/client/App.js
@@ -0,0 +1,11 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const react_1 = __importDefault(require("react"));
+const App = () => {
+    return (<>Skini placeholder v2</>);
+};
+exports.default = App;
+//# sourceMappingURL=App.js.map
--- dist/client/index.js
+++ dist/client/index.js
@@ -26,44 +26,9 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
-const path_1 = __importDefault(require("path"));
-const dotenv = __importStar(require("dotenv"));
-dotenv.config();
-const SephoraAPI_1 = __importDefault(require("./controllers/SephoraAPI"));
-const app = (0, express_1.default)();
-const PORT = 3000;
-(0, SephoraAPI_1.default)();
-app.use(express_1.default.json());
-app.get('/shop', (req, res) => {
-    if (req) {
-        console.log("shop endpoint reached");
-        res.status(200).json({});
-    }
-});
-app.get('/product', (req, res) => {
-    if (req) {
-        console.log("product endpoint reached");
-        res.status(200).json({});
-    }
-});
-app.get('/search', (req, res) => {
-    if (req) {
-        console.log("search endpoint reached");
-        res.status(200).json({});
-    }
-});
-app.get('/', (req, res) => {
-    if (req) {
-        (0, SephoraAPI_1.default)()
-            .then((result) => {
-            console.log("root endpoint reached");
-            res.status(200).send(result);
-        });
-    }
-});
-app.use('/', express_1.default.static(path_1.default.join(__dirname, "../public")));
-app.listen(PORT, () => {
-    console.log(`Listening on PORT: ${PORT}`);
-});
-//# sourceMappingURL=server.js.map
+const react_1 = __importDefault(require("react"));
+const ReactDOM = __importStar(require("react-dom/client"));
+const App_1 = __importDefault(require("./App"));
+const root = ReactDOM.createRoot(document.querySelector("#app"));
+root.render(<App_1.default />);
+//# sourceMappingURL=index.js.map
